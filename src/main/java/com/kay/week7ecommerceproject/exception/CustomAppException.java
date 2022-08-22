8e7c6bcf5023fd963febbce4816c0d2549e5c6a6
calebsamuelfc <samcalluis@gmail.com>
2022-08-22 10:57:13 +0100

    added features: CustomAppException

--- src/main/java/com/kay/week7ecommerceproject/exception/CustomAppException.java
+++ src/main/java/com/kay/week7ecommerceproject/exception/CustomAppException.java
@@ -2,6 +2,7 @@ package com.kay.week7ecommerceproject.exception;
 
 public class CustomAppException extends Exception{
     public CustomAppException(String message) {
+
         super(message);
     }
 }
